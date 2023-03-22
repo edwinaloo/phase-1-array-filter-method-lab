ea9d37abf4107196dc92d13d80b9da0f2a795e8a
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-22 18:36:19 +0300

    ~push

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// function to find matching drivers based on the given name
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // function to find drivers whose names start with the provided letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // function to match drivers based on their name property
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
 // Code your solution here
